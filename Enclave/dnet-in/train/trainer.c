4084d04da67a018d80c391d7a155b2eab213a125
Yuhala <petersonyuhala@gmail.com>
2020-02-14 17:33:26 +0100

    testing file i/o wrapper fxns

--- Enclave/dnet-in/train/trainer.c
+++ Enclave/dnet-in/train/trainer.c
@@ -15,10 +15,11 @@ void ecall_trainer(list *sections, data *training_data, int pmem)
     char buffer[100];
     ocall_close_file();//dont have fseek ocall so I close and reopen for now :-)
     ocall_open_file("file.txt");
-    
+
      /* Read and display data */
    fread(buffer, strlen(c)+1, 1, 0);
    printf("%s\n", buffer);
+    ocall_close_file();
 
     return;
 
