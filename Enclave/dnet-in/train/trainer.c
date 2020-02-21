db0d28ec4c040fc61eee5407fe0094b577bbafd4
Yuhala <petersonyuhala@gmail.com>
2020-02-21 08:41:16 +0100

    mnist full train and test

--- Enclave/dnet-in/train/trainer.c
+++ Enclave/dnet-in/train/trainer.c
@@ -55,8 +55,9 @@ void train_mnist(list *sections, data *training_data, int pmem)
         if (avg_loss == -1)
             avg_loss = loss;
         avg_loss = avg_loss * .95 + loss * .05;
-         printf("Batch num: %ld, Seen: %.3f: Loss: %f, Avg loss: %f avg, L. rate: %f rate,Images seen: %ld \n",
-               get_current_batch(net), (float)(*net->seen) / N, loss, avg_loss, get_current_rate(net), *net->seen);
+        progress = (get_current_batch(net) / net->max_batches) * 100;
+        printf("Batch num: %ld, Seen: %.3f: Loss: %f, Avg loss: %f avg, L. rate: %f rate,Progress: %f%% \n",
+               get_current_batch(net), (float)(*net->seen) / N, loss, avg_loss, get_current_rate(net), progress);
         if (*net->seen / N > epoch)
         {
             //TODO: save weights
